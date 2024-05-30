4ad02ac44b7c2b609ff77269056b00cdaf272da7
onlyasrenal <1174755932@qq.com>
2024-05-31 02:35:36 +0800

    final version

--- js/quiz.js
+++ js/quiz.js
@@ -1,30 +1,51 @@
 document.addEventListener('DOMContentLoaded', async function () {
-
-  let currentQuestionIndex
-  let correctAnswer
+  let currentQuestionIndex = 0
+  let correctAnswers = 0
+  let countdown
+  let result
+  let startTime
+  let endTime
+  sortedData = localStorage.getItem('myData')
+  console.log(sortedData)
+  if (sortedData) {
+    result = JSON.parse(sortedData)
+    console.log(result)
+  }
+  else {
+    result = []
+  }
+  let sortedResult = []
+  let username
   function startTimeCountdown() {
-    const time = document.getElementById('time')
-    let timeleft = 10
-    time.textContent = 'You have ' + timeleft-- + ' seconds to finish the quiz'
-    let countdown = setInterval(function () {
-      time.textContent = 'You have ' + timeleft-- + ' seconds to finish the quiz'
-      if (timeleft < 0) {
+    const timeElement = document.getElementById('time')
+    seconds = 10
+    timeElement.textContent = 'Time left: 10 s'
+    countdown = setInterval(() => {
+      seconds--
+      timeElement.textContent = 'Time left: ' + seconds + 's'
+      if (seconds <= 0) {
         clearInterval(countdown)
-        time.textContent = 'Time is over !'
+        timeElement.textContent = 'Time is over!'
         timeout()
       }
     }, 1000)
   }
 
   function timeout() {
-    console.log("Time's up!");
-    const quizData = data[currentQuestionIndex];
-    displayMessage(`Time's up! The correct answer was: ${quizData.answer}`);
-    // 3秒后进入下一题
+    console.log("Time's up!")
+    const Content = document.getElementById('content')
+    Content.textContent = `The correct answer was: ${data[currentQuestionIndex].answer}`
+    const removeButton = document.querySelector('.options ul');
+    removeButton.removeEventListener
+    removeButton.textContent = ''
     setTimeout(() => {
-      currentQuestionIndex++;
-      displayNextQuestion();
-    }, 3000);
+      const buttons = document.querySelectorAll('.options ul button');
+      buttons.forEach(button =>
+        button.removeEventListener('click', button.onclick)
+      )
+      currentQuestionIndex++
+      displayQuiz(data[currentQuestionIndex], currentQuestionIndex)
+    }, 3000)
   }
   const jsonUrl = './ques.json'
   try {
@@ -34,13 +55,11 @@ document.addEventListener('DOMContentLoaded', async function () {
       throw Error(`Error! status: ${response.status}`)
     }
     var data = await response.json()
-    console.log(data)
-    // startQuiz()
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error)
   }
   function displayQuiz(quizData, index) {
-    startTimeCountdown();
+    startTimeCountdown()
     const title = document.getElementById('title')
     const quizContent = document.getElementById('content')
     const optionsElement = document.querySelector('.options ul')
@@ -58,30 +77,63 @@ document.addEventListener('DOMContentLoaded', async function () {
       optionsElement.appendChild(li)
       button.textContent = Option
       button.addEventListener('click', function () {
-        const isCorrect = checkAnswer(this.textContent, quizData.answer);
-        const message = isCorrect ? 'Correct!' : `Wrong! The correct answer is: ${quizData.answer}`;
-        quizContent.innerHTML = message;
+        const userAnswer = this.textContent;
+        const isCorrect = checkAnswer(userAnswer, quizData.answer);
+
+        // 显示用户选择的答案是否正确
+        quizContent.innerHTML = isCorrect ? '<p>Correct!</p>' :
+          `<p>Wrong! You choose ${userAnswer} but the correct answer is: ${quizData.answer}</p>`;
 
         if (isCorrect) {
-          correctAnswers++; // 答对题目，增加计数器
+          correctAnswers++;
         }
+        const removeButton = document.querySelector('.options ul');
+        removeButton.removeEventListener
+        removeButton.innerHTML = '<button id = "nextbutton">next</button>'
+        clearInterval(countdown)
+        const nextButton = document.getElementById('nextbutton');
+        nextButton.addEventListener('click', () => {
+          quizContent.innerHTML = '';
 
-        // 无论答对答错，3秒后进入下一题
-        setTimeout(() => {
-          currentQuestionIndex = index + 1;
-          displayNextQuestion();
-        }, 3000);
-      });
+          // 检查是否还有更多问题
+          if (currentQuestionIndex < data.length - 1) {
+            currentQuestionIndex++;
+            displayQuiz(data[currentQuestionIndex], currentQuestionIndex);
+          } else {
+            // 测验结束，显示最终得分
+            endTime = new Date().getTime()
+            useTime = ((endTime - startTime) / 1000).toFixed(2)
+            let newResult = {
+              name: username,
+              score: correctAnswers,
+              time: useTime
+            }
+            result.push(newResult)
+            let sortedResult = result.slice().sort((a, b) => {
+              if (a.score == b.score) {
+                return a.time - b.time;
+              } else {
+                return b.score - a.score;
+              }
+            });
+            console.log(sortedResult)
+            updateRanking(sortedResult)
+            localStorage.setItem('myData', JSON.stringify(sortedResult))
+            quizContent.innerHTML = `<p>Quiz completed! You used ${useTime} seconds to get ${correctAnswers} out of ${data.length} answers correct.</p>`;
+            button.textContent = ''
+            removeButton.innerHTML = ''
+            li.appendChild(button)
+            optionsElement.appendChild(li)
+            button.innerHTML = 'Restart'
+            // restart
+            button.addEventListener('click', function () {
+              window.location.reload()
+            })
+          }
+        })
+      })
     })
   }
-  // function displayMessage(message) {
-  //   const messageElement = document.getElementById('quizResults');
-  //   messageElement.textContent = message;
-  //   // 3秒后清除消息
-  //   setTimeout(() => {
-  //     messageElement.textContent = '';
-  //   }, 3000);
-  // }
   function checkAnswer(textContent, answer) {
     if (textContent === answer) {
       return true
@@ -90,45 +142,49 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
   }
 
-  function displayNextQuestion() {
-    if (currentQuestionIndex < data.length) {
-      displayQuiz(data[currentQuestionIndex], currentQuestionIndex);
-    } else {
-      displayMessage(`Quiz completed! You got ${correctAnswers} answers correct.`);
-      // 重置计数器和索引
-      correctAnswers = 0;
-      currentQuestionIndex = 0;
-      // 可以在这里添加其他结束测验后的逻辑
-    }
-  }
-
   function startQuiz() {
-    // 重置计数器和索引
-    correctAnswers = 0;
-    currentQuestionIndex = 0;
-    // 显示第一个问题
-    displayQuiz(data[currentQuestionIndex], currentQuestionIndex);
+    correctAnswers = 0
+    currentQuestionIndex = 0
+    startTime = new Date().getTime()
+    displayQuiz(data[currentQuestionIndex], currentQuestionIndex)
   }
 
+  function updateRanking(data) {
+    console.log(data)
+    let rows = document.querySelectorAll('.rank table tbody tr');
+    data.slice(0, 10).forEach((item, index) => {
+      if (index < rows.length) {
+        //填充数据
+        rows[index].querySelector('td:nth-child(2)').textContent = item.name
+        rows[index].querySelector('td:nth-child(3)').textContent = item.score;
+        rows[index].querySelector('td:nth-child(4)').textContent = item.time
+      }
+    });
+  }
   function startAll() {
     const quizContent = document.getElementById('content')
     quizContent.innerHTML = ''
-    console.log('start1')
-    quizContent.innerHTML = '<div>Please enter you name</div><input type="text" id="nameInput" placeholder="Enter your name">'
+    quizContent.innerHTML = '<div>Please enter your name</div>'
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.id = 'nameInput'
+    nameInput.placeholder = 'Enter your name'
+
     const submitButton = document.createElement('button')
     submitButton.textContent = 'Submit'
     submitButton.id = 'submitButton'
+
+    quizContent.appendChild(nameInput)
     quizContent.appendChild(submitButton)
+    updateRanking(result)
     submitButton.addEventListener('click', function () {
-      let userName = document.getElementById('nameInput').value
-      console.log(userName)
-      if (userName) {
+      username = document.getElementById('nameInput').value
+      if (username) {
         startQuiz()
       } else {
         alert('Please enter your name.')
       }
     })
-
   }
   startAll()
 })
